5cf9cc160be61199d864733915553b28c6a491f0
Nishad Ahsan <nishadahsan@gmail.com>
2015-11-11 19:03:51 +0600

    Moved 'fortune cookies' functionality moved to module

--- meadowlark.js
+++ meadowlark.js
@@ -1,14 +1,7 @@
 var __dirName = '/home/nishad/visual Studio Code Files/meadowlark/site';
+var fortune = require('./lib/fortune.js');
 
 var express = require('express');
-
-var fortunes = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
 
 var app = express();
 
@@ -27,8 +20,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-	var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-	res.render('about', { fortune: randomFortune });
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
